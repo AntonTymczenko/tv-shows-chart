90c4817e33cb42c76a8c9cb2984545c024e6f7e6
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-01 21:08:41 +0300

    feat: Connect Store to ChartList

--- imports/components/ChartList.js
+++ imports/components/ChartList.js
@@ -1,30 +1,28 @@
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 import { Shows } from '/imports/api/collections';
 import ChartItem from './ChartItem'
 
-const ChartList = ({ shows }) =>
-  shows.length ? shows.map(show => (
+class ChartList extends Component {
+  render() {
+    return this.props.shows.length ? this.props.shows.map(show => (
       <ChartItem
         show={show}
         key={show._id}
       />
     )) : null
-
-export default withTracker(({ sort, page = 1, limit = 0 }) => {
-  Meteor.subscribe('shows')
-
-  const skip = limit * (page - 1)
-
-  const shows = Shows.find({}, {
-    sort,
-    skip,
-    limit,
-  }).fetch()
-
-  return {
-    shows,
   }
-})(ChartList)
+}
+
+const mapStateToProps = (state, props) => ({
+  limit: state.shows.limit,
+  totalCount: state.shows.totalCount,
+  pageMax: state.shows.pageMax,
+  page: state.shows.page,
+  shows: state.shows.data,
+})
+
+export default connect(mapStateToProps)(ChartList);
