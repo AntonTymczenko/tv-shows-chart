61e5b62d6bf4534e643aa935ba03c627fda106fa
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-02 16:26:32 +0300

    fix: Remove updates in ChartList component.
    Redux changes `shows.data` automatically. So no need in checking for 
    changes inside the component

--- imports/components/ChartList.js
+++ imports/components/ChartList.js
@@ -3,35 +3,9 @@ import { withTracker } from 'meteor/react-meteor-data';
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
 
-import { Shows } from '/imports/api/collections';
-import { setCurrentPage } from '/imports/actions/shows';
 import ChartItem from './ChartItem'
 
 class ChartList extends Component {
-  componentDidMount() {
-    this.props.dispatch(setCurrentPage(this.props.page))
-  }
-
-  componentDidUpdate(prevProps) {
-    const { page, totalCount, limit, pageMax } = this.props
-    const pageIndexIsPossible = page <= pageMax
-    const pageIndexHasChanged = page !== prevProps.page
-
-
-    const currentPageFirstIndex = page * limit
-    const currentPageLastIndex = ((page + 1) * limit) - 1
-    const maxIndex = totalCount - 1
-
-    const shownCount = currentPageFirstIndex + this.props.shows.length
-    const updatedDataHasImpactOnCurrentPage = shownCount < totalCount &&
-      maxIndex <= currentPageLastIndex || (maxIndex > currentPageLastIndex && shownCount !== limit)
-
-    const OKToUpdate = pageIndexIsPossible &&
-      pageIndexHasChanged ||
-      updatedDataHasImpactOnCurrentPage
-    if (OKToUpdate) this.props.dispatch(setCurrentPage(this.props.page))
-  }
-
   render() {
     return this.props.shows.length ? this.props.shows.map(show => (
       <ChartItem
@@ -43,10 +17,6 @@ class ChartList extends Component {
 }
 
 const mapStateToProps = (state, props) => ({
-  limit: state.shows.limit,
-  totalCount: state.shows.totalCount,
-  pageMax: state.shows.pageMax,
-  page: state.shows.page,
   shows: state.shows.data,
 })
 
