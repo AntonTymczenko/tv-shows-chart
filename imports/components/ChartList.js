b9c7147f669909854e887a781e83dfd587efb8fe
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-02 18:57:06 +0300

    refactor: Simplify ChartList

--- imports/components/ChartList.js
+++ imports/components/ChartList.js
@@ -1,20 +1,16 @@
 import { Meteor } from 'meteor/meteor';
-import { withTracker } from 'meteor/react-meteor-data';
-import React, { Component } from 'react'
+import React from 'react'
 import { connect } from 'react-redux';
 
 import ChartItem from './ChartItem'
 
-class ChartList extends Component {
-  render() {
-    return this.props.shows.length ? this.props.shows.map(show => (
+const ChartList = props =>
+  props.shows.length ? props.shows.map(show => (
       <ChartItem
         show={show}
         key={show._id}
       />
     )) : null
-  }
-}
 
 const mapStateToProps = (state, props) => ({
   shows: state.shows.data,
