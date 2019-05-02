7e2282f67a9a501915b2c9ab1e70d04adda88b0a
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-02 16:14:10 +0300

    fix: Don't fetch data on SET_CURRENT_PAGE, only change page index

--- imports/reducers/shows.js
+++ imports/reducers/shows.js
@@ -1,5 +1,3 @@
-import { Shows } from '/imports/api/collections';
-
 const showsReducerDefault = {
   sort: { watchers: -1 },
   limit: 20,
@@ -18,18 +16,8 @@ export default (shows = showsReducerDefault, action) => {
         pageMax: Math.floor(action.totalCount / shows.limit),
       }
     case 'SET_CURRENT_PAGE':
-      const { limit, sort } = shows
-      const skip = limit * page
-
-      const data = Shows.find({}, {
-        sort,
-        skip,
-        limit,
-      }).fetch()
-
       return {
         ...shows,
-        data,
         page: action.page,
       }
     default:
