54a081f65a7b7f6bec12f7681c235d9169d1a1e6
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-02 16:19:37 +0300

    refactor: Actions can use other actions and State

--- imports/actions/shows.js
+++ imports/actions/shows.js
@@ -1,9 +1,18 @@
-export const setTotalShowsCount = (totalCount = 0) => ({
-  type: 'SET_TOTAL_SHOWS_COUNT',
-  totalCount,
-})
 
-export const setCurrentPage = (page = 0) => ({
-  type: 'SET_CURRENT_PAGE',
-  page,
-})
+export const setTotalShowsCount = (totalCount = 0) => {
+  return (dispatch, getState) => {
+    dispatch ({
+      type: 'SET_TOTAL_SHOWS_COUNT',
+      totalCount,
+    })
+  }
+}
+
+export const setCurrentPage = (page = 0) => {
+  return dispatch => {
+    dispatch({
+      type: 'SET_CURRENT_PAGE',
+      page,
+    })
+  }
+}
