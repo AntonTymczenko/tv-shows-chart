0bdf821f872c8103a86b5b3725d8d7b9cb864d6c
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-03 23:42:49 +0300

    refactor: Simplify passing of arguments

--- imports/api/harvester/index.js
+++ imports/api/harvester/index.js
@@ -2,7 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import updatedLessThanMinutes from './updated-recently'
 import updateDatabase from './update-database'
 
-const updateOnce = (period = 0, { manual = false } = {}) => {
+const updateOnce = ({ period = 0, manual = false } = {}) => {
   if (manual) {
     updateDatabase({ manual })
   } else if (!updatedLessThanMinutes(period)) {
@@ -11,8 +11,8 @@ const updateOnce = (period = 0, { manual = false } = {}) => {
 }
 
 const updateEvery = period => {
-  updateOnce(period)
-  Meteor.setInterval(() => updateOnce(period), period * 60 * 1000)
+  updateOnce({ period })
+  Meteor.setInterval(() => updateOnce({ period }), period * 60 * 1000)
 }
 
 const UPDATE_PERIOD = parseInt(process.env.UPDATE_PERIOD) || 1440 // in minutes
@@ -20,7 +20,7 @@ const UPDATE_PERIOD = parseInt(process.env.UPDATE_PERIOD) || 1440 // in minutes
 Meteor.methods({
   updateDatabaseOnDemand() {
     // TODO: add option of update only current page
-    updateOnce(undefined, { manual: true })
+    updateOnce({ manual: true })
   }
 })
 
