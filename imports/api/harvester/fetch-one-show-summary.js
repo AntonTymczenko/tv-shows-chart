eb3ea93f624b82e1a457621d6a9f35ff6bd82596
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-04 14:00:59 +0300

    style: Indent whole promise

--- imports/api/harvester/fetch-one-show-summary.js
+++ imports/api/harvester/fetch-one-show-summary.js
@@ -16,29 +16,27 @@ const msgAPI = api => {
 const errMsg = (id, api) => `Error trying to update ${msgAPI(api)}${id}`
 
 export default ({ _id, ids }) => new Promise((resolve, reject) => {
-  // Trakt summary
-  const traktRequest = new Promise((resolve, reject) => {
+    // Trakt summary
+    const traktRequest = new Promise((resolve, reject) => {
+    const url = `https://api.trakt.tv/shows/${ids.trakt}`
+    const headers = httpHeaders.trakt
+    const query = 'extended=full'
+    HTTP.get(url, { headers, query }, (err, res) => {
+      if (err) return resolve({
+        ...err,
+        status: err.response && err.response.statusCode || 500,
+      })
 
-  const url = `https://api.trakt.tv/shows/${ids.trakt}`
-  const headers = httpHeaders.trakt
-  const query = 'extended=full'
-  HTTP.get(url, { headers, query }, (err, res) => {
-    if (err) return resolve({
-      ...err,
-      status: err.response && err.response.statusCode || 500,
+      Shows.update(
+        { _id },
+        { $set: res.data},
+        (err, res) => {
+          if (err) return reject(err)
+          if (res !== 1) return reject(new Error(errMsg(ids.trakt, 'trakt')))
+          resolve(_id)
+        }
+      )
     })
-
-    Shows.update(
-      { _id },
-      { $set: res.data},
-      (err, res) => {
-        if (err) return reject(err)
-        if (res !== 1) return reject(new Error(errMsg(ids.trakt, 'trakt')))
-        resolve(_id)
-      }
-    )
-  })
-
   })
 
   // TMDB additional info (poster, ...)
