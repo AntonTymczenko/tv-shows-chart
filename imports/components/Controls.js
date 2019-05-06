2cd24074c0ba9ab6ececebb9b7265ccf6b3cf99b
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-06 20:57:27 +0300

    fix: Remove manual DB update of DB from Client

--- imports/components/Controls.js
+++ imports/components/Controls.js
@@ -4,17 +4,8 @@ import { connect } from 'react-redux';
 
 import { fetchCurrentPage } from '/imports/actions/shows';
 
-const updateDB = cb => {
-  Meteor.call('updateDatabaseOnDemand', (err, res) => {
-    if (err) return console.error(err)
-    cb()
-  })
-}
 
 const Controls = props => (
-  <button onClick={() => updateDB(() =>
-    props.dispatch(fetchCurrentPage())
-  )}>UPDATE</button>
 )
 
 const mapStateToProps = (state, props) => ({
