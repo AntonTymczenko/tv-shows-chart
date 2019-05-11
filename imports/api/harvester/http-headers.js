179006feb18739c5653301281296df041345643c
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-11 19:10:42 +0300

    feat: Add HTTP headers for TMDB

--- imports/api/harvester/http-headers.js
+++ imports/api/harvester/http-headers.js
@@ -1,9 +1,13 @@
-const { TRAKT_CLIENT_ID } = process.env
+const { TRAKT_CLIENT_ID, TMDB_TOKEN } = process.env
 
 export default {
   trakt: {
       'Content-type': 'application/json',
       'trakt-api-key': TRAKT_CLIENT_ID,
       'trakt-api-version': 2,
+  },
+  tmdb: {
+    'Authorization': `Bearer ${TMDB_TOKEN}`,
+    'Content-Type': 'application/json;charset=utf-8',
   },
 }
