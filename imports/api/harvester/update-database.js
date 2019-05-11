7eab67910a2b9acd8e5c3d63f322260a9315199a
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-11 18:24:38 +0300

    feat: Write to log entry the time it took to update DB

--- imports/api/harvester/update-database.js
+++ imports/api/harvester/update-database.js
@@ -5,12 +5,15 @@ const errMsg = id => `Unsuccessful DB update. Read more in log entry ${id}`
 
 export default async ({ manual }) => {
   try {
+    const startTime = new Date()
     const res = await fetchTrending()
+    const updateTime = new Date() - startTime
 
     LogEntries.insert({
       ...res,
       manual,
       date: new Date(),
+      updateTime,
     }, (err, id) => {
       if (err) throw err
       if (res.status !== 200) throw new Error(errMsg(id))
