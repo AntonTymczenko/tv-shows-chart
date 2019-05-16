d7e3478f2465006a8a8b807fa74a8f8b5d4726d2
Anton Tymchenko <anton.tymchenko.firefly@gmail.com>
2019-05-16 22:19:33 +0300

    style: Indentation

--- imports/components/ChartItem/index.js
+++ imports/components/ChartItem/index.js
@@ -10,84 +10,84 @@ const roundedRating = rating => Math.round((rating || 0 ) * 100) / 100
 
 function ChartItem({show}) {
   return (
-  <div className="row chart__item">
-    <div
-      className={classNames('cell', 'cell_poster', {
-        'no-poster': !show.poster_path
-      })}
-    >
-      <img
-        className="poster"
-        src={show.poster_path || '/no-image.png'}
-        alt={show.poster_path ? show.title + ' poster' : 'No poster available'}
-      />
-    </div>
-    <div className="cell cell_watchers">
-      { show.watchers }
-    </div>
-    <div className="cell cell_rating">
-      { roundedRating(show.rating) }
-    </div>
-    <div className="cell cell_title">
-      { show.title }
-    </div>
-    <div className="cell cell_year">
-      { show.year }
-    </div>
-    <div className="cell cell_country">
-      <Country countryCode={show.country} />
-    </div>
-    <div className="cell cell_genres">
-      <p className="legend">Genres: </p>
-      <Genres genres={show.genres} />
-    </div>
-    <div className="cell cell_links">
-      <Links
-        title={show.title}
-        imdb={show.ids.imdb}
-        homepage={show.homepage}
-        trailer={show.trailer}
-      />
-    </div>
-    <div className="cell cell_compact">
-      <p>
-        <span className="compact-info">
-          <span className="compact-info__label">
-            Watchers:
-          </span>
-          { show.watchers }
-        </span>
-        <span className="compact-info">
-          <span className="compact-info__label">
-            Rating:
-          </span>
-          { roundedRating(show.rating) }
-        </span>
-      </p>
-      <p>
-        <span className="compact-info">
-          <span className="compact-info__label">
-            Year:
-          </span>
-          { show.year }
-        </span>
-        <span className="compact-info">
-          <span className="compact-info__label">
-            Country:
-          </span>
-          <Country countryCode={show.country} />
-        </span>
-      </p>
-      <p>
+    <div className="row chart__item">
+      <div
+        className={classNames('cell', 'cell_poster', {
+          'no-poster': !show.poster_path
+        })}
+      >
+        <img
+          className="poster"
+          src={show.poster_path || '/no-image.png'}
+          alt={show.poster_path ? show.title + ' poster' : 'No poster available'}
+        />
+      </div>
+      <div className="cell cell_watchers">
+        { show.watchers }
+      </div>
+      <div className="cell cell_rating">
+        { roundedRating(show.rating) }
+      </div>
+      <div className="cell cell_title">
+        { show.title }
+      </div>
+      <div className="cell cell_year">
+        { show.year }
+      </div>
+      <div className="cell cell_country">
+        <Country countryCode={show.country} />
+      </div>
+      <div className="cell cell_genres">
+        <p className="legend">Genres: </p>
+        <Genres genres={show.genres} />
+      </div>
+      <div className="cell cell_links">
         <Links
           title={show.title}
           imdb={show.ids.imdb}
           homepage={show.homepage}
           trailer={show.trailer}
         />
-      </p>
+      </div>
+      <div className="cell cell_compact">
+        <p>
+          <span className="compact-info">
+            <span className="compact-info__label">
+              Watchers:
+            </span>
+            { show.watchers }
+          </span>
+          <span className="compact-info">
+            <span className="compact-info__label">
+              Rating:
+            </span>
+            { roundedRating(show.rating) }
+          </span>
+        </p>
+        <p>
+          <span className="compact-info">
+            <span className="compact-info__label">
+              Year:
+            </span>
+            { show.year }
+          </span>
+          <span className="compact-info">
+            <span className="compact-info__label">
+              Country:
+            </span>
+            <Country countryCode={show.country} />
+          </span>
+        </p>
+        <p>
+          <Links
+            title={show.title}
+            imdb={show.ids.imdb}
+            homepage={show.homepage}
+            trailer={show.trailer}
+          />
+        </p>
+      </div>
     </div>
-  </div>
   )
 }
 
